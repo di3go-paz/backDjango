63f9069596e881fcca773e105d8e42531a9a9c4f
di3go-paz <diego.paz.maturana@gmail.com>
2025-07-07 22:47:13 -0400

    correccion de start.sh cambio de directorio BACKEND a config

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn BACKEND.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT

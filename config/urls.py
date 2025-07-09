6012e6697274b1703d05d3ae1e8ef246ac5e8912
di3go-paz <diego.paz.maturana@gmail.com>
2025-07-08 21:35:23 -0400

    correccion de ruta principal

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
 
 
 urlpatterns = [
+    path('', lambda request: redirect('admin:index')),
     path('admin/', admin.site.urls),
     path('users/', include('apps.users.urls')),
     path('inventory/', include('apps.inventory.urls')),
--- config/wsgi.py
+++ config/wsgi.py
@@ -6,14 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
 
 
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-
-if not User.objects.filter(username=os.environ.get('SUPERUSER_USERNAME')).exists():
-    User.objects.create_superuser(
-        os.environ.get('SUPERUSER_USERNAME'),
-        os.environ.get('SUPERUSER_EMAIL'),
-        os.environ.get('SUPERUSER_PASSWORD')
-    )
-    print("Superusuario creado!")

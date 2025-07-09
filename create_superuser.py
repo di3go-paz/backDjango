51d8d657a95d02ae237ed0364446c17f86db4962
di3go-paz <diego.paz.maturana@gmail.com>
2025-07-08 22:30:58 -0400

    correccion

--- create_superuser.py
+++ create_superuser.py
@@ -3,6 +3,7 @@ from apps.users.models import CustomUser
 CustomUser.objects.create_superuser(
     username="admin",
     password="admin123",
+    rol="admin",
     is_staff=True,
     is_superuser=True
 )

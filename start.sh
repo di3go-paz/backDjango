6fae17b810da001be1f1c33449241a1279939d18
di3go-paz <diego.paz.maturana@gmail.com>
2025-07-08 22:03:49 -0400

    correccionde creacion del super user

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python config/create_superuser.py
+python create_superuser.py
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT

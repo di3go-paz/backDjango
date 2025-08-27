48ade85ecbce30aaabd27992ef757bcf5c3d13a2
di3go-paz <diego.paz.maturana@gmail.com>
2025-08-27 13:10:08 -0400

    correccion de settings.py

--- config/settings.py
+++ config/settings.py
@@ -119,10 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/home/di3gopaz/erp/backDjango/staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/home/di3gopaz/erp/backDjango/media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

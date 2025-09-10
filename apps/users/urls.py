4192441f429584bc044dfab5d36ecd786040475c
di3go-paz <diego.paz.maturana@gmail.com>
2025-09-10 02:30:45 -0300

    creacion de token personalizado

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -14,6 +14,5 @@ router = DefaultRouter()
 router.register(r'usuarios', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path("api/token-auth/", CustomAuthToken.as_view(), name="token-auth"),  # Ruta para obtener token con rol
 ]  # Incluir en urls.py global como: path('/users/', include('apps.users.urls'))
